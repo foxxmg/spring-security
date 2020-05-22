7f0af4e0570d56eb783b04a00b5e5558e2c42c63
Farai Mugaviri <mugavirifarai@gmail.com>
2020-05-22 04:43:53 +0200

    written sql script for database support

---
+++ src/main/resources/spring_security.sql
@@ -0,0 +1,53 @@
+DROP DATABASE IF EXISTS `spring_security`;
+
+CREATE DATABASE  IF NOT EXISTS `spring_security`;
+USE `spring_security`;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `users`
+--
+
+INSERT INTO `users` 
+VALUES 
+('farai','{noop}1234',1),
+('john','{noop}1234',1),
+('lisa','{noop}1234',1);
+
+
+--
+-- Table structure for table `authorities`
+--
+
+DROP TABLE IF EXISTS `authorities`;
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `authorities`
+--
+
+INSERT INTO `authorities` 
+VALUES 
+('john','ROLE_EMPLOYEE'),
+('farai','ROLE_EMPLOYEE'),
+('farai','ROLE_MANAGER'),
+('lisa','ROLE_EMPLOYEE'),
+('lisa','ROLE_ADMIN');
+
+
